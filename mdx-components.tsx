05779db9f52b2a4acd8f9b5702624ad3836524b3
Matteo Badini <matteo.badini@gellify.com>
2025-01-31 16:13:05 +0100

    style: code highlight

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,76 +1,75 @@
-import React, { ComponentPropsWithoutRef } from 'react';
-import { Link } from 'next-view-transitions';
-import { highlight } from 'sugar-high';
+import React, { ComponentPropsWithoutRef } from "react";
+import { Link } from "next-view-transitions";
+import { highlight } from "sugar-high";
 
-type HeadingProps = ComponentPropsWithoutRef<'h1'>;
-type ParagraphProps = ComponentPropsWithoutRef<'p'>;
-type ListProps = ComponentPropsWithoutRef<'ul'>;
-type ListItemProps = ComponentPropsWithoutRef<'li'>;
-type AnchorProps = ComponentPropsWithoutRef<'a'>;
-type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
+type HeadingProps = ComponentPropsWithoutRef<"h1">;
+type ParagraphProps = ComponentPropsWithoutRef<"p">;
+type ListProps = ComponentPropsWithoutRef<"ul">;
+type ListItemProps = ComponentPropsWithoutRef<"li">;
+type AnchorProps = ComponentPropsWithoutRef<"a">;
+type BlockquoteProps = ComponentPropsWithoutRef<"blockquote">;
 
 const components = {
   h1: (props: HeadingProps) => (
-    <h1 
-      className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl mb-8 mt-2" 
-      {...props} 
+    <h1
+      className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl mb-8 mt-2"
+      {...props}
     />
   ),
   h2: (props: HeadingProps) => (
-    <h2 
-      className="scroll-m-20 border-b pb-2 text3xl font-semibold tracking-tight first:mt-0 mt-4 mb-2" 
-      {...props} 
+    <h2
+      className="scroll-m-20 border-b pb-2 text3xl font-semibold tracking-tight first:mt-0 mt-4 mb-2"
+      {...props}
     />
   ),
   h3: (props: HeadingProps) => (
-    <h3 
-      className="scroll-m-20 text-2xl font-semibold tracking-tight mt-4 mb-2" 
-      {...props} 
+    <h3
+      className="scroll-m-20 text-2xl font-semibold tracking-tight mt-4 mb-2"
+      {...props}
     />
   ),
   h4: (props: HeadingProps) => (
-    <h4 
-      className="scroll-m-20 text-xl font-semibold tracking-tight mt-6 mb-2" 
-      {...props} 
+    <h4
+      className="scroll-m-20 text-xl font-semibold tracking-tight mt-6 mb-2"
+      {...props}
     />
   ),
   p: (props: ParagraphProps) => (
-    <p 
-      className="leading-7 [&:not(:first-child)]:mt-6 text-muted-foreground" 
-      {...props} 
+    <p
+      className="leading-7 [&:not(:first-child)]:mt-6 text-muted-foreground"
+      {...props}
     />
   ),
   ol: (props: ListProps) => (
-    <ol 
-      className="my-6 ml-6 list-decimal [&>li]:mt-2 text-muted-foreground" 
-      {...props} 
+    <ol
+      className="my-6 ml-6 list-decimal [&>li]:mt-2 text-muted-foreground"
+      {...props}
     />
   ),
   ul: (props: ListProps) => (
-    <ul 
-      className="my-6 ml-6 list-disc [&>li]:mt-2 text-muted-foreground" 
-      {...props} 
+    <ul
+      className="my-6 ml-6 list-disc [&>li]:mt-2 text-muted-foreground"
+      {...props}
     />
   ),
-  li: (props: ListItemProps) => (
-    <li className="leading-7" {...props} />
-  ),
-  em: (props: ComponentPropsWithoutRef<'em'>) => (
+  li: (props: ListItemProps) => <li className="leading-7" {...props} />,
+  em: (props: ComponentPropsWithoutRef<"em">) => (
     <em className="font-medium" {...props} />
   ),
-  strong: (props: ComponentPropsWithoutRef<'strong'>) => (
+  strong: (props: ComponentPropsWithoutRef<"strong">) => (
     <strong className="font-medium" {...props} />
   ),
   a: ({ href, children, ...props }: AnchorProps) => {
-    const className = 'font-medium underline underline-offset-4 text-primary hover:text-primary/80';
-    if (href?.startsWith('/')) {
+    const className =
+      "font-medium underline underline-offset-4 text-primary hover:text-primary/80";
+    if (href?.startsWith("/")) {
       return (
         <Link href={href} className={className} {...props}>
           {children}
         </Link>
       );
     }
-    if (href?.startsWith('#')) {
+    if (href?.startsWith("#")) {
       return (
         <a href={href} className={className} {...props}>
           {children}
@@ -89,13 +88,13 @@ const components = {
       </a>
     );
   },
-  code: ({ children, ...props }: ComponentPropsWithoutRef<'code'>) => {
+  code: ({ children, ...props }: ComponentPropsWithoutRef<"code">) => {
     const codeHTML = highlight(children as string);
     return (
-      <code 
+      <code
         className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
-        dangerouslySetInnerHTML={{ __html: codeHTML }} 
-        {...props} 
+        dangerouslySetInnerHTML={{ __html: codeHTML }}
+        {...props}
       />
     );
   },
