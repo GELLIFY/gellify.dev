f3a855c6b10fa3a42d9776f18661dfacfd877746
Matteo Badini <matteo.badini@gellify.com>
2025-06-13 15:18:25 +0200

    feat: keep v1

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,44 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout, DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 
+const layoutProps: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  sidebar: {
+    hideSearch: false,
+    collapsible: true,
+    tabs: [
+      {
+        title: "Template v2",
+        description: "The latest version",
+        url: "/docs/v2",
+        icon: <img
+        src="https://raw.githubusercontent.com/colinhacks/zod/3782fe29920c311984004c350b9fefaf0ae4c54a/logo.svg"
+        alt="Zod 3"
+        className="h-6"
+      />,
+      },
+      {
+        title: "Template v1",
+        description: "Deprecated",
+        url: "/docs/v1",
+        icon: (
+          <img
+            src="https://raw.githubusercontent.com/colinhacks/zod/3782fe29920c311984004c350b9fefaf0ae4c54a/logo.svg"
+            alt="Zod 3"
+            className="h-6"
+          />
+        ),
+      },
+    ],
+  },
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout {...layoutProps} {...baseOptions}>
       {children}
     </DocsLayout>
   );
