7c8e1bfe8a76d0ad2fb33f94dea3506251324c23
Matteo Badini <matteo.badini@gellify.com>
2025-06-17 15:50:16 +0200

    feat: new doc structure

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/docs',
+        destination: '/',
+        permanent: true,
+      },
+      
+    ]
+  },
 };
 
 export default withMDX(config);
--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -7,32 +7,7 @@ const layoutProps: DocsLayoutProps = {
   ...baseOptions,
   tree: source.pageTree,
   sidebar: {
-    hideSearch: false,
     collapsible: true,
-    tabs: [
-      {
-        title: "Template v2",
-        description: "The latest version",
-        url: "/docs/v2",
-        icon: <img
-        src="https://raw.githubusercontent.com/colinhacks/zod/3782fe29920c311984004c350b9fefaf0ae4c54a/logo.svg"
-        alt="Zod 3"
-        className="h-6"
-      />,
-      },
-      {
-        title: "Template v1",
-        description: "Deprecated",
-        url: "/docs/v1",
-        icon: (
-          <img
-            src="https://raw.githubusercontent.com/colinhacks/zod/3782fe29920c311984004c350b9fefaf0ae4c54a/logo.svg"
-            alt="Zod 3"
-            className="h-6"
-          />
-        ),
-      },
-    ],
   },
 };
 
